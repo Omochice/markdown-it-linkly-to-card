2deaf7b35165f7faa942021316a4e000bd140b5b
Omochice <mochice.mls.ntl@gmail.com>
2024-05-16 01:57:30 +0900

    feat: add sample impl

--- mod.ts
+++ mod.ts
@@ -6,6 +6,28 @@ import type {
 // NOTE: import with `mjs` is needed
 import Token from "npm:markdown-it@14.1.0/lib/token.mjs";
 
+const replaceLink = (token: Token) => {
+  if (token.type !== "inline") {
+    return;
+  }
+
+  const children = token.children;
+
+  if (children == null) {
+    return;
+  }
+
+  if (!URL.canParse(token.content)) {
+    return;
+  }
+
+  const newToken = new Token("html_inline", "", 0);
+  newToken.content = "<a>I replaced this</a>";
+  token.children = [
+    newToken,
+  ];
+};
+
 const linkifyToCard: PluginSimple = (
   md: MarkdownIt,
   options?: MarkdownOptions,
@@ -14,27 +36,7 @@ const linkifyToCard: PluginSimple = (
     "replacements",
     "linkly-to-card",
     ({ tokens }: { tokens: Token[] }) => {
-      for (const token of tokens) {
-        if (token.type !== "inline") {
-          return;
-        }
-
-        const children = token.children;
-
-        if (children == null) {
-          return;
-        }
-
-        if (!URL.canParse(token.content)) {
-          return;
-        }
-
-        const newToken = new Token("html_inline", "", 0);
-        newToken.content = "<a>I replaced this</a>";
-        token.children = [
-          newToken,
-        ];
-      }
+      tokens.forEach(replaceLink);
       return true;
     },
   );
